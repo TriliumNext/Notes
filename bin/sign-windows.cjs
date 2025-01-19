f8bbbbd8e43e5fb4574c4666bd39138acdbf84fe
Elian Doran <contact@eliandoran.me>
2025-01-19 20:04:35 +0200

    fix(ci): use right command

--- bin/sign-windows.cjs
+++ bin/sign-windows.cjs
@@ -2,6 +2,7 @@ const child_process = require("child_process");
 const SIGN_EXECUTABLE = "C:\\ev_signer_trilium\\ev_signer_trilium.exe";
 
 module.exports = function (filePath) {
-    console.log(`Path to file to sign: ${filePath}`);
-    child_process.execSync(`${SIGN_EXECUTABLE} "${filePath}"`);
+    const command = `${SIGN_EXECUTABLE} --executable "${filePath}"`;
+    console.log(`> ${command}`);
+    child_process.execSync(command);
 }
