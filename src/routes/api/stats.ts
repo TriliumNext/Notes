49f6f0dd602c341f38b568ed8897f8d0e7786c65
SiriusXT <1160925501@qq.com>
2025-04-01 18:29:44 +0800

    To prevent search lag when there are a large number of notes

--- src/routes/api/stats.ts
+++ src/routes/api/stats.ts
@@ -51,11 +51,7 @@ function getSubtreeSize(req: Request) {
 // Get the total number of notes
 function getNotesCount(req: Request) {
     const notesCount = sql.getRow(
-        `SELECT COUNT(*) AS count 
-FROM notes 
-WHERE isDeleted = 0 
-AND noteId NOT LIKE '|_%' ESCAPE '|';
-`,
+        `SELECT COUNT(*) AS count FROM notes WHERE isDeleted = 0;`,
     ) as { count: number };
     return notesCount.count;
 }
